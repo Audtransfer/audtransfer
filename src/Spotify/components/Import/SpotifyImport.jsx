babacf069ec026ee961f042d1f7b8dc7c550da38
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2022-03-26 11:59:09 -0300

    update Import

--- src/Spotify/components/Import/SpotifyImport.jsx
+++ src/Spotify/components/Import/SpotifyImport.jsx
@@ -5,46 +5,45 @@ import { useHistory, useLocation } from "react-router";
 const userEndpoint = "https://api.spotify.com/v1/me";
 
 export default function SpotifyImport() {
-  const data = JSON.parse(sessionStorage.getItem('playlisToTransfer'))
+  const data = JSON.parse(sessionStorage.getItem("playlisToTransfer"));
   const access_token = useLocation().state.access_token;
-  const [playlistPostId, setPlaylistPostId] = useState("")
-  const [user, setUser] = useState()
-  const history = useHistory()
+  const [user, setUser] = useState();
+  const history = useHistory();
 
   useEffect(() => {
-    axios.get(userEndpoint, { headers: { Authorization: "Bearer " + access_token } })
-      .then(response => { setUser(response.data) })
-      .catch(err => { console.log(err) });
-  }, [access_token])
+    axios.get(userEndpoint, { headers: { Authorization: "Bearer " + access_token }})
+		.then(response => setUser(response.data))
+		.catch(err => console.log(err));
+  }, [access_token]);
 
   const handleCreate = () => {
     let today = new Date();
-    axios.post(`https://api.spotify.com/v1/users/${user.id}/playlists`,
-      {
-        "name": data.playlistName,
-        "description": `Playlist created by: Audtransfer - ${today}`,
-        "public": true
-      },
-      { headers: { Authorization: "Bearer " + access_token } })
-      .then(response => { setPlaylistPostId(response.data.id) })
-      .catch(err => console.log(err));
-  }
+		today = today.toLocaleString()
+    axios.post(
+			`https://api.spotify.com/v1/users/${user.id}/playlists`,
+			{
+				name: data.playlistName,
+				description: `Audtransfer created this at: ${today}`,
+				public: true,
+			},
+			{ headers: { Authorization: "Bearer " + access_token } }
+		)
+		.then((response) => {
+			let tracksUrl = "";
+			const urlBase = `https://api.spotify.com/v1/playlists/${response.data.id}/tracks?uris=`;
+			data.tracks.map((item) => {
+				return (tracksUrl += "spotify%3Atrack%3A" + item.trackId + "%2C");
+			});
+			const urlForPost = `${urlBase}${tracksUrl}`;
 
-  const handleTransfer = () => {
-    let tracksUrl = ""
-    const urlBase = `https://api.spotify.com/v1/playlists/${playlistPostId}/tracks?uris=`
-    data.tracks.map(item => {
-      return tracksUrl += "spotify%3Atrack%3A" + item.trackId + "%2C"
-    })
-    const urlForPost = `${urlBase}${tracksUrl}`;
-
-    axios.post(urlForPost, null, { headers: { Authorization: "Bearer " + access_token } })
-      .then(() => {
-        sessionStorage.clear()
-        history.push("/success") //TODO
-      })
-      .catch(err => console.log(err));
-  }
+			axios.post(urlForPost, null, {headers: { Authorization: "Bearer " + access_token }})
+			.then(() => {
+				sessionStorage.clear();
+				history.push("/success");
+			});
+		})
+		.catch((err) => console.log(err));
+  };
 
   return (
     <div className="create wrapper">
@@ -52,19 +51,18 @@ export default function SpotifyImport() {
         <>
           <div className="create-buttons">
             <button onClick={handleCreate}>Create the Playlist</button>
-            {playlistPostId && <button onClick={handleTransfer}>Transfer the Playlist</button>}
           </div>
 
           <h3 className="create-title">Playlist name: {data.playlistName}</h3>
           <ul className="playlist-table seamless">
-            {data.tracks.map(item =>
+            {data.tracks.map((item) => (
               <li className="playlist-table__item" key={item.trackId}>
                 {item.trackName} - {item.artistName}
               </li>
-            )}
+            ))}
           </ul>
         </>
       )}
     </div>
-  )
+  );
 }
