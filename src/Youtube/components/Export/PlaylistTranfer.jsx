7f01c3e139cf52317a14f0d22fb88e50ed3b7c5e
Leonardo Falcão <leonardopfalcao1@gmail.com>
2022-04-22 21:26:28 -0300

    add: components youtube music e youtube page

--- src/Youtube/components/Export/PlaylistTranfer.jsx
+++ src/Youtube/components/Export/PlaylistTranfer.jsx
@@ -1,2 +1,31 @@
+import { useHistory } from "react-router";
+
 export default function PlaylistTranfer({ selected }) {
+	const history = useHistory()
+
+	const handleClick = () => {
+		const playlistToTransfer = ({
+			playlistId: selected.id,
+			playlistName: selected.name,
+			playlistType: selected.type,
+			playlistOrigin: "YouTube Music",
+			public: selected.public,
+			tracks: selected.tracks.items.map(item => {
+				return {
+					trackName: item.track.name,
+					artistName: item.track.artists[0].name,
+					trackId: item.track.id
+				}
+			}),
+		})
+		console.log(playlistToTransfer);
+		sessionStorage.setItem('playlisToTransfer', JSON.stringify(playlistToTransfer));
+		history.push("/");
+	}
+
+	return (
+		<>
+			<button onClick={() => handleClick()} className="button-submit">Transfer this playlist</button>
+		</>
+	)
 }
--- src/Youtube/components/Export/TrackLists.jsx
+++ src/Youtube/components/Export/TrackLists.jsx
@@ -1,2 +1,40 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import PlaylistTranfer from "./PlaylistTranfer"
+import { useYoutubeContext } from "../../../contexts/Youtube";
+
+const getPlaylistEndPoint = "https://www.googleapis.com/youtube/v3/playlists/"
+
 export default function TrackLists({ id }) {
+	const { accessToken } = useYoutubeContext();
+	const [selectedPlaylist, setSelectedPlaylist] = useState()
+
+  useEffect(() => {
+    axios.get(`${getPlaylistEndPoint}${id}`, { headers: { Authorization: "Bearer " + accessToken } })
+      .then(response => { setSelectedPlaylist(response.data) })
+      .catch(err => { console.log(err) });
+  }, [accessToken, id, setSelectedPlaylist])
+
+  return (
+    <>
+      {selectedPlaylist && (
+        <>
+          <PlaylistTranfer selected={selectedPlaylist} />
+          <div className="playlist">
+            <img className="playlist-image" src={selectedPlaylist.images[0].url} alt="Profile" />
+            <div className="playlist-info">
+              <p className="playlist-info_name">Playlist name: {selectedPlaylist.name}</p>
+              <p className="playlist-info_owner">Owner: {selectedPlaylist.owner.display_name}</p>
+            </div>
+
+            <ul className="playlist-table seamless">
+              {selectedPlaylist.tracks.items.map(item =>
+                <li className="playlist-table__item" key={item.track.id}>{item.track.name}</li>
+              )}
+            </ul>
+          </div>
+        </>
+      )}
+    </>
+  )
 }
