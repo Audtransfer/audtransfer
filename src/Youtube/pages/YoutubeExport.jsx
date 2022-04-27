511b812798851e1dac9667e70806ae20a6e41cd5
Gui-T-Fernandez <guilhermetoledofernandez@gmail.com>
2022-04-27 01:36:14 -0300

    getting user channel data on console.log

--- src/Youtube/pages/YoutubeExport.jsx
+++ src/Youtube/pages/YoutubeExport.jsx
@@ -1,3 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useYoutubeContext } from "../../contexts/Youtube";
+import TrackLists from "../components/Export/TrackLists";
+
+const youtubeEndpoint = "https://youtube.googleapis.com/youtube/v3";
+const youtubeUserEndpoint = `${youtubeEndpoint}/channels`;
+const youtubePlaylistEndpoint = `${youtubeEndpoint}/playlists`;
+
 export default function YoutubeExport() {
-    return null;
+    const { accessToken } = useYoutubeContext();
+	const [user, setUser] = useState({})
+	const [data, setData] = useState({})
+	const [selectedPlaylist, setSelectedPlaylist] = useState()
+
+	useEffect(() => {
+		axios.get(`${youtubeUserEndpoint}?part=snippet&mine=true`, {headers:{Authorization: "Bearer " + accessToken}})
+		.then(response => {
+            console.log(response.data);
+			//axios.get(`${youtubePlaylistEndpoint}?part=snippet&mine=true`, {headers:{Authorization: "Bearer " + accessToken}})
+			//.then(response => {setData(response.data)})
+		})
+		.catch(err => { console.log(err) });
+	},[accessToken])
+
+	return (<>
+	</>)
 }
