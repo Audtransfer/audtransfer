82b6fdbc17d588867521997b8c6f8e29b3908bd0
Leonardo Falcão <leonardopfalcao1@gmail.com>
2022-04-28 00:24:07 -0300

    add: puxando imagem e nome do usuário logado

--- src/Youtube/pages/YoutubeExport.jsx
+++ src/Youtube/pages/YoutubeExport.jsx
@@ -8,21 +8,31 @@ const youtubeUserEndpoint = `${youtubeEndpoint}/channels`;
 const youtubePlaylistEndpoint = `${youtubeEndpoint}/playlists`;
 
 export default function YoutubeExport() {
-    const { accessToken } = useYoutubeContext();
-	const [user, setUser] = useState({})
-	const [data, setData] = useState({})
-	const [selectedPlaylist, setSelectedPlaylist] = useState()
+  const { accessToken } = useYoutubeContext();
+  const [user, setUser] = useState({})
+  const [data, setData] = useState({})
+  const [selectedPlaylist, setSelectedPlaylist] = useState()
 
-	useEffect(() => {
-		axios.get(`${youtubeUserEndpoint}?part=snippet&mine=true`, {headers:{Authorization: "Bearer " + accessToken}})
-		.then(response => {
-            console.log(response.data);
-			//axios.get(`${youtubePlaylistEndpoint}?part=snippet&mine=true`, {headers:{Authorization: "Bearer " + accessToken}})
-			//.then(response => {setData(response.data)})
-		})
-		.catch(err => { console.log(err) });
-	},[accessToken])
+  useEffect(() => {
+    axios.get(`${youtubeUserEndpoint}?part=snippet&mine=true`, { headers: { Authorization: "Bearer " + accessToken } })
+    .then(response => setUser(response.data.items[0]))
+    .catch(err => console.log(err));
+  }, [accessToken])
 
-	return (<>
-	</>)
+  return (<>
+    {accessToken && (
+      <div className="wrapper export">
+        {user.snippet && (
+          <div className="user">
+            {user.snippet.thumbnails.default && (
+              <img className="user_img" src={user.snippet.thumbnails.default.url} alt="Profile" />
+            )}
+            <h3 className="user_name">
+              Hi, {user.snippet.title}
+            </h3>
+          </div>
+        )}
+      </div>
+    )}
+  </>)
 }
