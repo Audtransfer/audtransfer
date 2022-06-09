b2b52bf97632ccd6780a386a12b12c61d91bd0fe
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2022-06-09 02:56:58 -0300

    update youtubeImport

--- src/Youtube/pages/YoutubeImport.jsx
+++ src/Youtube/pages/YoutubeImport.jsx
@@ -1,163 +1,213 @@
 import axios from "axios";
 import { useHistory } from "react-router";
 import { useYoutubeContext } from "../../contexts/Youtube";
+import { useState } from "react";
 
 const basicEndpoint = "https://youtube.googleapis.com/youtube/v3";
 
 export default function YoutubeImport() {
-	const dataTransfer = JSON.parse(sessionStorage.getItem('playlisToTransfer'));
-	const { accessToken } = useYoutubeContext();
-	const history = useHistory();
+  const dataTransfer = JSON.parse(sessionStorage.getItem("playlisToTransfer"));
+  const { accessToken } = useYoutubeContext();
 
-	const handleCreate = () => {
-		let today = new Date();
-		today = today.toLocaleString("pt-BR");
-		axios.post(
-				`${basicEndpoint}/playlists?part=snippet%2Cstatus`,
-				{
-					snippet: {
-					  	title: dataTransfer.playlistName,
-					  	description: `Audtransfer created this at: ${today}`,
-					},
-					status: {
-					  	privacyStatus: dataTransfer.public? "public" : "private"
-					}
+  const handleCreate = () => {
+    let today = new Date();
+    today = today.toLocaleString("pt-BR");
+    axios.post(
+			`${basicEndpoint}/playlists?part=snippet%2Cstatus`,
+			{
+				snippet: {
+					title: dataTransfer.playlistName,
+					description: `Audtransfer created this at: ${today}`,
 				},
-				{headers: {Authorization: "Bearer " + accessToken }}
-			)
-			.then(async response => {
-				console.log(response.data);
-				console.log(new Date());
-				await new Promise(r => setTimeout(r, 10000));
-				const urlBase = `${basicEndpoint}/playlistItems?part=snippet`;
-
-				if(dataTransfer.playlistOrigin === "YouTube") {
-					console.log("same origin");
-					dataTransfer.tracks.map(async item => {
-						console.log(new Date());
-						console.log(item.trackId);
-						await new Promise(r => setTimeout(r, 3000));
-						await axios.post(
-							urlBase, 
-							{
-								snippet: {
-									playlistId: response.data.id,
-									resourceId: {
-										kind: "youtube#video",
-										videoId: item.trackId
-									}
-								}
-							},
-							{headers: {Authorization: "Bearer " + accessToken }})
-							.then(response => console.log(response.data))
-							.catch((err) => console.log(err));
-					});
-				}
-				else {
-					console.log("foreign origin");
-					dataTransfer.tracks.map(async item => {
-						let vidID = await handleSearch(item);
-						console.log(new Date());
-						console.log(vidID);
-						await new Promise(r => setTimeout(r, 3000));
-						await axios.post(
-							urlBase, 
-							{
-								snippet: {
-									playlistId: response.data.id,
-									resourceId: {
-										kind: "youtube#video",
-										videoId: vidID
-									}
-								}
-							},
-							{headers: {Authorization: "Bearer " + accessToken }})
-							.then(response => console.log(response.data))
-							.catch((err) => console.log(err));
-					});
-				}
-		
-				sessionStorage.clear();
-				history.push("/success");
+				status: { privacyStatus: dataTransfer.public ? "public" : "private", },
+			},
+			{ headers: { Authorization: "Bearer " + accessToken } }
+		)
+		.then(async (response) => {
+			console.log("id nova playlist:\n", response.data);
+			
+			let tracksPromises = dataTransfer.tracks.map(async item => {
+				let trackId = await handleSearch(item)
+				return trackId
 			})
-			.catch((err) => console.log(err));
-	};
+			let tracks = (await Promise.all(tracksPromises))
+			console.log("arrays de tracks ids: ", tracks);
 
-	const handleSearch = async (item) => {
-		const { data } = await axios.get(
-			`${basicEndpoint}/search?part=snippet&maxResults=50&q=${item.trackName}%20${item.artistName}&type=video`,
-			{headers: {Authorization: "Bearer " + accessToken}}
-		);
+			const urlBase = `${basicEndpoint}/playlistItems?part=snippet`;
 
-        if (
-            data.items.find(x => 
-            x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, '') === (item.artistName.toUpperCase() + " - TOPIC").replace(/[^\w\s]/gi, '')
-            && x.snippet.title.toUpperCase().replace(/[^\w\s]/gi, '').includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, '')))
-            )
-            return data.items.find(x => 
-                x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, '') === (item.artistName.toUpperCase() + " - TOPIC").replace(/[^\w\s]/gi, '')
-                && x.snippet.title.toUpperCase().replace(/[^\w\s]/gi, '').includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, ''))
-                ).id.videoId;
+			let post = await tracks.map(async item => {
+				let postPlaylist = await axios.post(urlBase, {
+					snippet: {
+						playlistId: response.data.id,
+						resourceId: { 
+							kind: "youtube#video",
+							videoId: item
+						}
+					}
+				}, 
+				{ headers: { Authorization: "Bearer " + accessToken }})
 
-        if (
-            data.items.find(x => 
-            x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, '') === (item.artistName.toUpperCase() + " VEVO").replace(/[^\w\s]/gi, '')
-            && x.snippet.title.toUpperCase().replace(/[^\w\s]/gi, '').includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, '')))
-            )
-            return data.items.find(x => 
-                x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, '') === (item.artistName.toUpperCase() + " VEVO").replace(/[^\w\s]/gi, '')
-                && x.snippet.title.toUpperCase().replace(/[^\w\s]/gi, '').includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, ''))
-                ).id.videoId;
+				return postPlaylist
+			})
+			let tracksPosted = (await Promise.all(post))
+			console.log(tracksPosted);
 
-        if (
-            data.items.find(x => 
-            x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, '') === item.artistName.toUpperCase().replace(/[^\w\s]/gi, '')
-            && x.snippet.title.toUpperCase().replace(/[^\w\s]/gi, '').includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, '')))
-            )
-            return data.items.find(x => 
-                x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, '') === item.artistName.toUpperCase().replace(/[^\w\s]/gi, '')
-                && x.snippet.title.toUpperCase().replace(/[^\w\s]/gi, '').includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, ''))
-                ).id.videoId;
+			/*
+			sessionStorage.clear();
+			history.push("/success");
+			*/
+		})
+		.catch((err) => console.log(err));
+  };
 
-        if (
-            data.items.find(x => 
-            x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, '').startsWith(item.artistName.toUpperCase().replace(/[^\w\s]/gi, ''))
-            && x.snippet.title.toUpperCase().replace(/[^\w\s]/gi, '').includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, '')))
-            )
-            return data.items.find(x => 
-                x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, '').startsWith(item.artistName.toUpperCase().replace(/[^\w\s]/gi, ''))
-                && x.snippet.title.toUpperCase().replace(/[^\w\s]/gi, '').includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, ''))
-                ).id.videoId;
+  const handleSearch = async (item) => {
+    const { data } = await axios.get(
+      `${basicEndpoint}/search?part=snippet&maxResults=50&q=${item.trackName}%20${item.artistName}&type=video`,
+      { headers: { Authorization: "Bearer " + accessToken } }
+    );
 
-        if (
-            data.items.find(x => 
-            x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, '').startsWith(item.artistName.toUpperCase().replace(/[^\w\s]/gi, '')))
-            )
-            return data.items.find(x => 
-                x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, '').startsWith(item.artistName.toUpperCase().replace(/[^\w\s]/gi, ''))
-                ).id.videoId;
+    if (
+      data.items.find(
+        (x) =>
+          x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, "") ===
+            (item.artistName.toUpperCase() + " - TOPIC").replace(
+              /[^\w\s]/gi,
+              ""
+            ) &&
+          x.snippet.title
+            .toUpperCase()
+            .replace(/[^\w\s]/gi, "")
+            .includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, ""))
+      )
+    )
+      return data.items.find(
+        (x) =>
+          x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, "") ===
+            (item.artistName.toUpperCase() + " - TOPIC").replace(
+              /[^\w\s]/gi,
+              ""
+            ) &&
+          x.snippet.title
+            .toUpperCase()
+            .replace(/[^\w\s]/gi, "")
+            .includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, ""))
+      ).id.videoId;
 
-		return data.items[0].id.videoId;
-	};
-  
-	return (
-		<div className="create wrapper">
-		  {dataTransfer && (
-			<>
-			  <div className="create-buttons">
-				<button onClick={handleCreate}>Create the Playlist</button>
-			  </div>
-	
-			  <h3 className="create-title">Playlist name: {dataTransfer.playlistName}</h3>
-			  <ul className="playlist-table seamless">
-				{dataTransfer.tracks?.map((item) => (
-				  <li className="playlist-table__item" key={item.trackId}>
-					{item.trackName} - {item.artistName}
-				  </li>
-				))}
-			  </ul>
-			</>
-		  )}
-		</div>
-	);
+    if (
+      data.items.find(
+        (x) =>
+          x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, "") ===
+            (item.artistName.toUpperCase() + " VEVO").replace(
+              /[^\w\s]/gi,
+              ""
+            ) &&
+          x.snippet.title
+            .toUpperCase()
+            .replace(/[^\w\s]/gi, "")
+            .includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, ""))
+      )
+    )
+      return data.items.find(
+        (x) =>
+          x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, "") ===
+            (item.artistName.toUpperCase() + " VEVO").replace(
+              /[^\w\s]/gi,
+              ""
+            ) &&
+          x.snippet.title
+            .toUpperCase()
+            .replace(/[^\w\s]/gi, "")
+            .includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, ""))
+      ).id.videoId;
+
+    if (
+      data.items.find(
+        (x) =>
+          x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, "") ===
+            item.artistName.toUpperCase().replace(/[^\w\s]/gi, "") &&
+          x.snippet.title
+            .toUpperCase()
+            .replace(/[^\w\s]/gi, "")
+            .includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, ""))
+      )
+    )
+      return data.items.find(
+        (x) =>
+          x.snippet.channelTitle.toUpperCase().replace(/[^\w\s]/gi, "") ===
+            item.artistName.toUpperCase().replace(/[^\w\s]/gi, "") &&
+          x.snippet.title
+            .toUpperCase()
+            .replace(/[^\w\s]/gi, "")
+            .includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, ""))
+      ).id.videoId;
+
+    if (
+      data.items.find(
+        (x) =>
+          x.snippet.channelTitle
+            .toUpperCase()
+            .replace(/[^\w\s]/gi, "")
+            .startsWith(
+              item.artistName.toUpperCase().replace(/[^\w\s]/gi, "")
+            ) &&
+          x.snippet.title
+            .toUpperCase()
+            .replace(/[^\w\s]/gi, "")
+            .includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, ""))
+      )
+    )
+      return data.items.find(
+        (x) =>
+          x.snippet.channelTitle
+            .toUpperCase()
+            .replace(/[^\w\s]/gi, "")
+            .startsWith(
+              item.artistName.toUpperCase().replace(/[^\w\s]/gi, "")
+            ) &&
+          x.snippet.title
+            .toUpperCase()
+            .replace(/[^\w\s]/gi, "")
+            .includes(item.trackName.toUpperCase().replace(/[^\w\s]/gi, ""))
+      ).id.videoId;
+
+    if (
+      data.items.find((x) =>
+        x.snippet.channelTitle
+          .toUpperCase()
+          .replace(/[^\w\s]/gi, "")
+          .startsWith(item.artistName.toUpperCase().replace(/[^\w\s]/gi, ""))
+      )
+    )
+      return data.items.find((x) =>
+        x.snippet.channelTitle
+          .toUpperCase()
+          .replace(/[^\w\s]/gi, "")
+          .startsWith(item.artistName.toUpperCase().replace(/[^\w\s]/gi, ""))
+      ).id.videoId;
+
+    return data.items[0].id.videoId;
+  };
+
+  return (
+    <div className="create wrapper">
+      {dataTransfer && (
+        <>
+          <div className="create-buttons">
+            <button onClick={handleCreate}>Create the Playlist</button>
+          </div>
+
+          <h3 className="create-title">
+            Playlist name: {dataTransfer.playlistName}
+          </h3>
+          <ul className="playlist-table seamless">
+            {dataTransfer.tracks?.map((item) => (
+              <li className="playlist-table__item" key={item.trackId}>
+                {item.trackName} - {item.artistName}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
 }
