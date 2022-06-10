5e758265bad8ecf38150920b69241706c73a74df
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2022-06-10 20:00:20 -0300

    Uncomment for PR

--- src/Deezer/pages/DeezerImport.jsx
+++ src/Deezer/pages/DeezerImport.jsx
@@ -51,8 +51,8 @@ export default function DeezerImport() {
 		})
 		.then(response => {
 			console.log(response);
-			// sessionStorage.clear();
-			// history.push("/success");
+			sessionStorage.clear();
+			history.push("/success");
 		})
 		.catch(err => console.log(err));
 	}
